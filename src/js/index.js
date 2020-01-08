284f77180f9e27fe7edb59627672f1ccae7a6d92
Kevin Castillo <kevincastillo318@gmail.com>
2020-01-08 20:12:27 +0000

    bare project completed

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,21 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-// window.onload = () => {
-//   let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
-//   let numbersCard = numbers[Math.floor(Math.random() * numbers.length)];
-//   return numbersCard;
-// };
+window.onload = () => {
+  document.querySelector(".card").classList.add(randomSuit());
+  document.querySelector(".card").innerHTML = randomFace();
+};
 
-// document.querySelector(".face").innerHTML = numbersCard;
+let randomFace = () => {
+  let face = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let faceCard = [Math.floor(Math.random() * face.length)];
+
+  return face[faceCard];
+};
+
+let randomSuit = () => {
+  let suit = ["diamond", "heart", "spade", "club"];
+  let suitCard = [Math.floor(Math.random() * suit.length)];
+
+  return suit[suitCard];
+};
