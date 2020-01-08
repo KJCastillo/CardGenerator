39802ee420f893aa10e20c77f7818632403e75c7
Kevin Castillo <kevincastillo318@gmail.com>
2020-01-08 19:42:05 +0000

    css complete

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,10 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = () => {
+//   let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K"];
+//   let numbersCard = numbers[Math.floor(Math.random() * numbers.length)];
+//   return numbersCard;
+// };
+
+// document.querySelector(".face").innerHTML = numbersCard;
